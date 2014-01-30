20873444602f95d0a5a81d56dee1fba6dee005b7
Jacquot-SFE <byron.jacquot@sparkfun.com>
2014-01-29 17:08:55 -0700

    More code cleanup

--- software/VKey_demo/VKey_demo.ino
+++ software/VKey_demo/VKey_demo.ino
@@ -1,28 +1,39 @@
 /*
- 12-10-2015
+ 1-29-2014
  Spark Fun Electronics
  Byron Jacquot
  
  This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
  
- This demonstrates decoding the input from the VKey voltage keypad.
+ This demonstrates interfacing the VKey voltage keypad using the VKey library.
+ 
+ The VKey has an array of 12 pushbuttons, each producing a unique analog voltage when 
+ pushed.  A microcontroller can read the voltagge, and determine which key has been pressed.  The 
+ VKey library keeps track of the analog pin connection, key status, and voltage calculation details.fs
+ 
+ To use the library, instantiate a VKey object and periodically check for input using the 
+ checkKeys() function.
 */
 
 #include <Arduino.h>
 #include <VKey.h>
 
-
+// Global declaration of the VKey class
+// Initialized with analog pin number and supply voltage
 VKey keypad(1, VKey::FIVE );
 
 void setup()
 {
+  
   Serial.begin(9600);
-  Serial.println("VKey example");
+  Serial.println("Welcome to VKey example");
+  
+  // No VKey specific initialization required.
 }
 
 void loop() 
 {
-  VKey::eKeynum k;
+  VKey::eKeynum k;  // Variable to receive the 
 
   if(keypad.checkKeys(k))
   {
@@ -30,11 +41,13 @@ void loop()
     // The boolean return value indicates whether that value is different than the
     // previous value.
     
-    // Only print when key has changed
+    // Only print when value has changed
     Serial.print("Got key: ");
     Serial.println(k);
   }
 
+  // The responsiveness of the keypad depends on how frequently it is checked. 
+  // 50 milliseconds seems to be a reasonable poll interval.
   delay(50);
 }
 
